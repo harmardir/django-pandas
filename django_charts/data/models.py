172fddb5246f3ec9bb08934a305c8ee5f1f9a78f
Harout <harmardir@gmail.com>
2022-09-13 23:09:38 +0300

    adding models

--- django_charts/data/models.py
+++ django_charts/data/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Purchase(models.Model):
+    city = models.CharField(max_length=50)
+    customer_type = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    unit_price = models.FloatField()
+    quantity = models.IntegerField()
+    product_line = models.CharField(max_length=50)
+    tax = models.FloatField()
+    total = models.FloatField()
+    date = models.DateField()
+    time = models.TimeField()
+    payment	= models.CharField(max_length=50)
+    cogs = models.FloatField()
+    profit = models.FloatField()
+    rating  = models.FloatField()
