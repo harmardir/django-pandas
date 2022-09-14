0f26c3e176d8f297d7129eb4789e896c89a05ce3
Harout <harmardir@gmail.com>
2022-09-14 23:14:54 +0300

    adding view and URLs

--- django_charts/data/views.py
+++ django_charts/data/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .methods import csv_to_db
 
-# Create your views here.
+class Dashboard(TemplateView):
+    template_name = 'dashboard.html'
+    def get_context_data(self, **kwargs):
+         # get the data from the default method       
+        context = super().get_context_data(**kwargs)
+        csv_to_db()
